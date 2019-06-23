26ecd0d9ce87159c1bf0171679bf38ad6b445fe4
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2019-06-23 18:18:16 +0800

    Intial Commit

---
+++ src/MongoDB.Serializer.ValueTuple/Provider/ValueTupleSerializerProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using MongoDB.Bson.Serialization;
+using MongoDB.Serializer.ValueTuple.Serializer;
+
+namespace MongoDB.Serializer.ValueTuple.Provider
+{
+    public class ValueTupleSerializerProvider : IBsonSerializationProvider
+    {
+        public IBsonSerializer GetSerializer(Type type)
+        {
+            if (type.IsGenericType && !type.ContainsGenericParameters)
+            {
+                var genericType = type.GetGenericTypeDefinition();
+
+                if (genericType == typeof(ValueTuple<>))
+                {
+                    var valueTupleSerializer = typeof(ValueTupleSerializer<>).MakeGenericType(type.GenericTypeArguments);
+                    return GetValueTupleSerializer(valueTupleSerializer);
+                }
+                else if (genericType == typeof(ValueTuple<,>))
+                {
+                    var valueTupleSerializer = typeof(ValueTupleSerializer<,>).MakeGenericType(type.GenericTypeArguments);
+                    return GetValueTupleSerializer(valueTupleSerializer);
+                }
+            }
+            return null;
+        }
+
+        private static IBsonSerializer GetValueTupleSerializer(Type valueTupleSerializer)
+        {
+            var valueTupleSerializerInfo = valueTupleSerializer.GetTypeInfo();
+
+            var valueTupleSerializerConstructor = valueTupleSerializerInfo.GetConstructor(new[] { typeof(IBsonSerializerRegistry) });
+            if (valueTupleSerializerConstructor is null)
+            {
+                throw new MissingMethodException("No constructor found for ValueTuple serializer");
+            }            
+            return valueTupleSerializerConstructor.Invoke(new object[] { BsonSerializer.SerializerRegistry }) as IBsonSerializer;
+        }
+    }
+}
---
+++ src/MongoDB.Serializer.ValueTuple/Serializer/ValueTupleSerializer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace MongoDB.Serializer.ValueTuple.Serializer
+{
+    public class ValueTupleSerializer<T1> : SerializerBase<ValueTuple<T1>>
+    {
+        private readonly Lazy<IBsonSerializer<T1>> _lazyItem1Serializer;
+
+        public ValueTupleSerializer(IBsonSerializerRegistry serializerRegistry)
+        {
+            if (serializerRegistry is null)
+            {
+                throw new ArgumentNullException("serializerRegistry");
+            }
+
+            _lazyItem1Serializer = new Lazy<IBsonSerializer<T1>>(() => serializerRegistry.GetSerializer<T1>());
+        }
+
+        public override ValueTuple<T1> Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            context.Reader.ReadStartArray();
+            var item1 = _lazyItem1Serializer.Value.Deserialize(context);
+            context.Reader.ReadEndArray();
+
+            return new ValueTuple<T1>(item1);
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, ValueTuple<T1> value)
+        {
+            context.Writer.WriteStartArray();
+            _lazyItem1Serializer.Value.Serialize(context, args, value.Item1);
+            context.Writer.WriteEndArray();
+        }
+    }
+
+    public class ValueTupleSerializer<T1, T2> : SerializerBase<ValueTuple<T1, T2>>
+    {
+        private readonly Lazy<IBsonSerializer<T1>> _lazyItem1Serializer;
+        private readonly Lazy<IBsonSerializer<T2>> _lazyItem2Serializer;
+
+
+        public ValueTupleSerializer(IBsonSerializerRegistry serializerRegistry)
+        {
+            if (serializerRegistry is null)
+            {
+                throw new ArgumentNullException("serializerRegistry");
+            }
+
+            _lazyItem1Serializer = new Lazy<IBsonSerializer<T1>>(() => serializerRegistry.GetSerializer<T1>());
+            _lazyItem2Serializer = new Lazy<IBsonSerializer<T2>>(() => serializerRegistry.GetSerializer<T2>());
+        }
+
+        public override (T1, T2) Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            context.Reader.ReadStartArray();
+            var item1 = _lazyItem1Serializer.Value.Deserialize(context);
+            var item2 = _lazyItem2Serializer.Value.Deserialize(context);
+            context.Reader.ReadEndArray();
+
+            return (item1, item2);
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, (T1, T2) value)
+        {
+            context.Writer.WriteStartArray();
+            _lazyItem1Serializer.Value.Serialize(context, args, value.Item1);
+            _lazyItem2Serializer.Value.Serialize(context, args, value.Item2);
+            context.Writer.WriteEndArray();
+        }
+    }
+}
