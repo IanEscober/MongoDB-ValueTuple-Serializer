cc9e600ac3df451115aa6062c1b50e06810f2f61
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2019-06-24 18:11:08 +0800

    Added simple and complex tests on initializing the ValueTuple Serializer

--- tests/MongoDB.Serializer.ValueTuple.Tests/Serializer/ValueTupleSerializerInitializeTests.cs
+++ tests/MongoDB.Serializer.ValueTuple.Tests/Serializer/ValueTupleSerializerInitializeTests.cs
@@ -2,6 +2,7 @@
 using Xunit;
 using MongoDB.Bson.Serialization;
 using MongoDB.Serializer.ValueTuple.Serializer;
+using MongoDB.Serializer.ValueTuple.Tests.Data;
 
 namespace MongoDB.Serializer.ValueTuple.Tests.Serializer
 {
@@ -9,11 +10,41 @@ namespace MongoDB.Serializer.ValueTuple.Tests.Serializer
     public class ValueTupleSerializerInitializeTests
     {
         [Fact]
-        public void Constructor_with_registry_should_initialize_instance()
+        public void Constructor_with_registry_should_initialize_instance_Simple_T1()
         {
             var mockSerializerRegistry = new Mock<IBsonSerializerRegistry>();
 
-            void act() => new ValueTupleSerializer<bool>(mockSerializerRegistry.Object);
+            void act() => new ValueTupleSerializer<SimpleValueTupleModel>(mockSerializerRegistry.Object);
+
+            act();
+        }
+
+        [Fact]
+        public void Constructor_with_registry_should_initialize_instance_Simple_Tn()
+        {
+            var mockSerializerRegistry = new Mock<IBsonSerializerRegistry>();
+
+            void act() => new ValueTupleSerializer<(SimpleValueTupleModel, bool?)>(mockSerializerRegistry.Object);
+
+            act();
+        }
+
+        [Fact]
+        public void Constructor_with_registry_should_initialize_instance_Complex_T1()
+        {
+            var mockSerializerRegistry = new Mock<IBsonSerializerRegistry>();
+
+            void act() => new ValueTupleSerializer<ComplexValueTupleModel>(mockSerializerRegistry.Object);
+
+            act();
+        }
+
+        [Fact]
+        public void Constructor_with_registry_should_initialize_instance_Complex_Tn()
+        {
+            var mockSerializerRegistry = new Mock<IBsonSerializerRegistry>();
+
+            void act() => new ValueTupleSerializer<(ComplexValueTupleModel, bool?)>(mockSerializerRegistry.Object);
 
             act();
         }
