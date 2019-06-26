b1a9552e5cfed6b0a4a285fc263d28164bf99e48
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2019-06-26 12:49:44 +0800

    Added ValueTuple serilizer provider tests

---
+++ tests/MongoDB.Serializer.ValueTuple.Tests/Provider/ValueTupleSerializerProviderTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+using MongoDB.Bson.Serialization;
+using MongoDB.Serializer.ValueTuple.Provider;
+
+namespace MongoDB.Serializer.ValueTuple.Tests.Provider
+{
+    [Collection("Registration collection")]
+    public class ValueTupleSerializerProviderTests
+    {
+        [Theory]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(TimeSpan))]
+        [InlineData(typeof(Tuple<bool>))]
+        public void GetSerializer_should_return_null(Type typeShouldNull)
+        {
+            var provider = new ValueTupleSerializerProvider();
+
+            var result = provider.GetSerializer(typeShouldNull);
+
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData(typeof(ValueTuple<bool>))]
+        [InlineData(typeof(ValueTuple<bool,bool>))]
+        [InlineData(typeof(ValueTuple<bool,bool, bool>))]
+        [InlineData(typeof(ValueTuple<bool, bool, bool, bool>))]
+        [InlineData(typeof(ValueTuple<bool, bool, bool, bool, bool>))]
+        [InlineData(typeof(ValueTuple<bool, bool, bool, bool,bool, bool>))]
+        [InlineData(typeof(ValueTuple<bool, bool, bool, bool, bool, bool, bool>))]
+        [InlineData(typeof(ValueTuple<bool, bool, bool, bool, bool, bool, bool, ValueTuple<bool>>))]
+        public void GetSerializer_should_return_value(Type typeShouldValue)
+        {
+            var provider = new ValueTupleSerializerProvider();
+
+            var result = provider.GetSerializer(typeShouldValue);
+
+            Assert.NotNull(result);
+        }
+    }
+}
--- tests/MongoDB.Serializer.ValueTuple.Tests/RegistrationFixture.cs
+++ tests/MongoDB.Serializer.ValueTuple.Tests/RegistrationFixture.cs
@@ -1,13 +1,11 @@
-﻿using MongoDB.Bson.Serialization;
-using MongoDB.Serializer.ValueTuple.Provider;
-
+﻿
 namespace MongoDB.Serializer.ValueTuple.Tests
 {
     public class RegistrationFixture
     {
         public RegistrationFixture()
         {
-            BsonSerializer.RegisterSerializationProvider(new ValueTupleSerializerProvider());
+            ValueTupleSerializerRegistry.Register();
         }
     }
 }
